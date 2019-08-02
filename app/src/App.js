b07d3650b8f8ea6e923c8bf456bf8eb8eb8d52aa
realNasirMalik <realNasirMalik@gmail.com>
2019-08-01 21:38:32 -0500

    :bento: Add 3 checkboxes
    Created <App /> component that renders 3 checkboxes with paragraphs next to it.
    
    • Initialized the React app
    • Rendered an <App /> component
    • Created the <App /> component from scratch

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+         <div>
+                <input type="checkbox" />
+                <p>Placeholder</p>
+
+                <input type="checkbox" />
+                <p>Placeholder</p>
+
+                <input type="checkbox" />
+                <p>Placeholder</p>
+         </div>
+  );
+}
+
+export default App;
