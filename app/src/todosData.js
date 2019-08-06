c31522f8ba139f07774f94447a0912cbffbee509
realNasirMalik <realNasirMalik@gmail.com>
2019-08-05 19:56:24 -0500

    :bento: Add mapping and props
    Used mapping and props to turn <TodoItem /> components into dynamic components that can display their own data. Essentially, this means that I'm declaratively using raw data and easily turning it into something that gets displayed on the screen.

---
+++ app/src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Organize desktop",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Clean room",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Clean bathroom",
+        completed: false
+    }
+]
+
+export default todosData
