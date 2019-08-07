5dc088587a98bcde109bca17bd5afd6273137973
realNasirMalik <realNasirMalik@gmail.com>
2019-08-06 20:04:28 -0500

    :building_construction: Implement the logic to change the state
    Implemented the logic to change the completed state of whichever todo item clicked.

--- app/src/App.js
+++ app/src/App.js
@@ -8,10 +8,31 @@ class App extends React.Component {
               this.state = {
                      todos: todosData
               }
+              this.handleChange = this.handleChange.bind(this)
+       }
+
+       handleChange(id) {
+              this.setState(prevState => {
+                     const updateTodos = prevState.todos.map(todo => {
+                            if (todo.id === id) {
+                                   todo.completed = !todo.completed
+                            }
+                            return todo
+                     })
+                     return {
+                            todos: updateTodos
+                     }
+              })
        }
 
        render() {
-              const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+              const todoItems = this.state.todos.map(
+                     item => <TodoItem
+                                   key={item.id}
+                                   item={item}
+                                   handleChange={this.handleChange}
+                              />
+              )
 
               return (
                      <div className="todo-list">
